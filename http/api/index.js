b29163847ac49fd3947a46a960e6bbb8e76080f9
Daniel Vieira <62304465+danielvieiralopes@users.noreply.github.com>
2020-05-24 03:16:24 -0300

    fim

--- http/api/index.js
+++ http/api/index.js
@@ -22,6 +22,7 @@ function writeFile(cb){
 
 http.createServer((req, res) => {
     const {name, url, del} = URL.parse(req.url, true).query
+    
     res.writeHead(200, {
         'Access-Control-Allow-Origin': '*'
     })
